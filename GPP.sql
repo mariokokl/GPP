9be03743ff1686a49b3764bec77066f192a10bc1
mariokokl <nosferatu.kokl@gmail.com>
2017-02-26 15:57:41 +0100

    Update GPP.sql

--- GPP.sql
+++ GPP.sql
@@ -1,10 +1,3 @@
--- --------------------------------------------------------
--- Poslužitelj:                  127.0.0.1
--- Server version:               5.6.16 - MySQL Community Server (GPL)
--- Server OS:                    Win32
--- HeidiSQL Verzija:             9.3.0.4984
--- --------------------------------------------------------
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8mb4 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
